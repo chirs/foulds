a732b9cbbdc8d75bf1225e28fcb00a5d9a76aece
Chris Edgemon <chrisedgemon@gmail.com>
2013-11-28 11:56:55 -0500

    Add ROOT_DIR.

--- settings.py
+++ settings.py
@@ -1,10 +1,21 @@
 import os
+import socket
 
 USER_AGENT = 'Mozilla/5.0 (Windows NT 5.1; rv:8.0; en_us) Gecko/20100101 Firefox/8.0'
 
-IMAGE_DIR = "/home/chris/www/foulds/images"
+host = socket.gethostname()
 
-CACHE_FOLDER = "/home/chris/www/scraper-cache"
+roots = {
+    'agni.local': '/Users/chris/soccer',
+    'bert': '/home/chris/www',
+    }
+
+ROOT_DIR = roots[host]
+
+
+IMAGE_DIR = os.path.join(ROOT_DIR, 'foulds/images')
+
+CACHE_FOLDER = os.path.join(ROOT_DIR, 'scraper-cache')
 PAGE_CACHE_FOLDER = os.path.join(CACHE_FOLDER, 'page')
 DATA_CACHE_FOLDER = os.path.join(CACHE_FOLDER, 'data')
 
