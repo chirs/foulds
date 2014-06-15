3d713752d2a5fd29d88f8ba7116b4f2093461c4c
Chris Edgemon <chrisedgemon@gmail.com>
2014-06-14 20:22:34 -0400

    add kicker

---
+++ sites/kicker.py
@@ -0,0 +1,8 @@
+#!/usr/local/bin/env python
+# -*- coding: utf-8 -*-
+
+import datetime
+import re
+
+from foulds.utils import scrape_soup, get_contents
+from foulds.cache import data_cache, set_cache
