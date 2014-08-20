5af64a49d0a6d93d89e9841a939cfffbc3296cad
Chris Edgemon <chrisedgemon@gmail.com>
2014-08-20 11:33:29 -0400

    add oscar to roots

--- settings.py
+++ settings.py
@@ -9,6 +9,7 @@ roots = {
     'agni.local': '/Users/chris/soccer',
     'agni': '/Users/chris/soccer',
     'bert': '/home/chris/www',
+    'oscar': '/home/chris/soccer',
     }
 
 ROOT_DIR = roots[host]
